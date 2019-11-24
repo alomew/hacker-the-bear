6e86035093a891bd96d5e17ab12b4f1b50a0ef0c
Mathew Allington <funkymallington@gmail.com>
2019-11-24 05:10:50 +0000

    push

--- backend/main.py
+++ backend/main.py
@@ -14,20 +14,20 @@ bot = MitsukuBot()
 datastore_client = datastore.Client()
 
 def store_speech(speech_json):
-    entity = datastore.Entity(key=datastore_client.key('speech'))
-    dt = datetime.datetime.now()
-    speech_json['timestamp'] = dt
-    entity.update(speech_json)
+	entity = datastore.Entity(key=datastore_client.key('speech'))
+	dt = datetime.datetime.now()
+	speech_json['timestamp'] = dt
+	entity.update(speech_json)
 
-    datastore_client.put(entity)
+	datastore_client.put(entity)
 
 def fetch_speeches(limit):
-    query = datastore_client.query(kind="speech")
-    query.order = ['-timestamp']
+	query = datastore_client.query(kind="speech")
+	query.order = ['-timestamp']
 
-    speeches = query.fetch(limit=limit)
+	speeches = query.fetch(limit=limit)
 
-    return speeches
+	return speeches
 
 # @app.route('/')
 # def root():
@@ -42,26 +42,26 @@ def fetch_speeches(limit):
 
 @app.route('/')
 def root():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route('/readspeeches')
 def fetchspeeches():
-    return jsonify(list(fetch_speeches(10)))
+	return jsonify(list(fetch_speeches(10)))
 
 @app.route('/addspeech', methods=['POST'])
 def addspeech():
-    json = request.get_json()
-    if json:
-        store_speech(json)
-        return jsonify(True)
-    else:
-        return jsonify(False)
+	json = request.get_json()
+	if json:
+		store_speech(json)
+		return jsonify(True)
+	else:
+		return jsonify(False)
 
 @app.route('/textofperson', methods=['POST'])
 def textofperson():
-    json = request.get_json()
-    print(json)
-    #json = {'user_id': "1", 'person_text': 'Hi there', 'timestamp': datetime.datetime.now().isoformat()}
+	json = request.get_json()
+	print(json)
+	#json = {'user_id': "1", 'person_text': 'Hi there', 'timestamp': datetime.datetime.now().isoformat()}
 	badWords = {"sad","lonely","unhappy","kill","die", "leave", "depressed","death","hungry","tired","stiff","aching", "miss"}
 	sosWords = {"fallen", "hurt", "injured", "broken", "pain", "ouch", "help", "stroke", "heart", "attack"}
 	happyWords = {"happy", "excited", "nice", "sunny"}
@@ -72,8 +72,8 @@ def textofperson():
 	forgetWords = {"forget", "forgotten"}
 	rand = random.randint(1,100)
 	# If 5 or less, tell them to drink water
-    if json:
-        try:
+	if json:
+		try:
 			person_text = json["person_text"]
 			status = 0
 			for word in person_text:
@@ -104,31 +104,31 @@ def textofperson():
 					break
 				
 			if status != 1:
-            	bear_text = bot.sendMessage(person_text)[1:-1]
+				bear_text = bot.sendMessage(person_text)[1:-1]
 			else:
 				bear_text = "Do you want some help?"
-        except:
-            bear_text = "I need some sleep."
-        json['bear_text'] = bear_text
-        return jsonify(json)
+		except:
+			bear_text = "I need some sleep."
+		json['bear_text'] = bear_text
+		return jsonify(json)
 
-    return jsonify({'user_id': '1',
-                    'person_text':'',
-                    'timestamp':datetime.datetime.now().isoformat(),
-                    'bear_text':'You have made a development error.'})
+	return jsonify({'user_id': '1',
+					'person_text':'',
+					'timestamp':datetime.datetime.now().isoformat(),
+					'bear_text':'You have made a development error.'})
 
 
 
 @app.route('/ping')
 def ping():
-    return jsonify('pong')
+	return jsonify('pong')
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+	# This is used when running locally only. When deploying to Google App
+	# Engine, a webserver process such as Gunicorn will serve the app. This
+	# can be configured by adding an `entrypoint` to app.yaml.
+	# Flask's development server will automatically serve static files in
+	# the "static" directory. See:
+	# http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+	# App Engine itself will serve those files as configured in app.yaml.
+	app.run(host='127.0.0.1', port=8080, debug=True)
