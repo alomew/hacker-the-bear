1835246ea79593f8f078d7fb4db88a073a5d4b13
alomew <alomew@gmail.com>
2019-11-24 05:25:31 +0000

    Transitioned to message creation service

--- backend/src/Main.elm
+++ backend/src/Main.elm
@@ -1,56 +1,68 @@
 module Main exposing (..)
 
 import Browser
-import Element.Input exposing (labelHidden, labelLeft)
+import Element exposing (centerX, column, el, padding, shrink, spacing, width)
+import Element.Font
+import Element.Input exposing (labelAbove, labelHidden, labelLeft)
 import Html exposing (Html)
 import Http
 import Json.Encode
-import Element exposing (column)
+
 
 main =
-  Browser.element { init = init, update = update, view = view , subscriptions = subscriptions}
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
-type alias Model = {user_id: String, person_text: String, bear_text: String}
+
+type alias Model =
+    { message : String, queuedMessages : List String, summary : String }
+
 
 encodeModel : Model -> Json.Encode.Value
 encodeModel model =
     Json.Encode.object <|
-        [ ( "user_id", Json.Encode.string model.user_id )
-        , ( "person_text", Json.Encode.string model.person_text )
-        , ( "bear_text", Json.Encode.string model.bear_text )
+        [ ( "message", Json.Encode.string model.message )
         ]
 
-init : () -> (Model, Cmd Msg)
-init _ = (Model "" "" "", Cmd.none)
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "" [] "", Cmd.none )
+
 
 type Msg
-    = UpdatedId String
-    | UpdatedPersonText String
-    | UpdatedChatText String
+    = UpdatedMessage String
     | Submitted
     | Received (Result Http.Error ())
 
-subscriptions _ = Sub.none
 
-update : Msg -> Model -> (Model, Cmd Msg)
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UpdatedId newId -> ({model | user_id = newId}, Cmd.none)
+        UpdatedMessage s ->
+            ( { model | message = s }, Cmd.none )
 
-        UpdatedPersonText newText -> ({model | person_text = newText}, Cmd.none)
+        Submitted ->
+            ( { model | message = "", queuedMessages = model.message :: model.queuedMessages }
+            , Cmd.none
+            )
 
-        UpdatedChatText newText -> ({model | bear_text = newText}, Cmd.none)
-
-        Submitted -> (Model "" "" ""
-                     , Http.post { url = "/addspeech", body = Http.jsonBody <| encodeModel model, expect = Http.expectWhatever Received})
-
-        Received _ -> (model, Cmd.none)
+        -- Http.post { url = "/addspeech", body = Http.jsonBody <| encodeModel model, expect = Http.expectWhatever Received})
+        Received _ ->
+            ( model, Cmd.none )
 
 
 view : Model -> Html Msg
-view model = Element.layout [] (column [] [ Element.Input.text [] {onChange = UpdatedId, text = model.user_id, placeholder = Nothing, label = labelLeft [] (Element.text "ID")}
-                                      , Element.Input.text [] {onChange = UpdatedPersonText, text = model.person_text, placeholder = Nothing, label = labelLeft [] (Element.text "Person Text")}
-                                      , Element.Input.text [] {onChange = UpdatedChatText, text = model.bear_text, placeholder = Nothing, label = labelLeft [] (Element.text "Bear text")}
-                                      , Element.Input.button [] {onPress = Just Submitted, label = Element.text "Submit"}
-                                      ])
-
+view model =
+    Element.layout
+        []
+        (column [ centerX, width shrink, spacing 15 ]
+            [ el [ Element.Font.size 25, centerX, width shrink, padding 10 ] (Element.text "Messages so far")
+            , column [ centerX, width shrink ] <| List.map (\m -> Element.text m) model.queuedMessages
+            , Element.Input.text [ centerX, width shrink ] { onChange = UpdatedMessage, text = model.message, placeholder = Nothing, label = labelAbove [] (Element.text "New message") }
+            , Element.Input.button [ centerX, width shrink ] { onPress = Just Submitted, label = Element.text "Submit" }
+            ]
+        )
